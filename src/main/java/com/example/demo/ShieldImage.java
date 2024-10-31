178acc5331da8b04d3e9e475deaa1a6a87a6aaae
jkgithub02 <jkwork02@gmail.com>
2024-10-31 14:57:27 +0800

    fixed nullPointerException

--- src/main/java/com/example/demo/ShieldImage.java
+++ src/main/java/com/example/demo/ShieldImage.java
@@ -3,16 +3,18 @@ package com.example.demo;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.util.Objects;
+
 public class ShieldImage extends ImageView {
 	
-	private static final String IMAGE_NAME = "/images/shield.png";
+	private static final String IMAGE_NAME = "/com/example/demo/images/shield.png";
 	private static final int SHIELD_SIZE = 200;
 	
 	public ShieldImage(double xPosition, double yPosition) {
 		this.setLayoutX(xPosition);
 		this.setLayoutY(yPosition);
 		//this.setImage(new Image(IMAGE_NAME));
-		this.setImage(new Image(getClass().getResource("/com/example/demo/images/shield.jpg").toExternalForm()));
+		this.setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_NAME)).toExternalForm()));
 		this.setVisible(false);
 		this.setFitHeight(SHIELD_SIZE);
 		this.setFitWidth(SHIELD_SIZE);
