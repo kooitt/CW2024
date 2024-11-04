33ef6c41041f4c850abf1f95cd694771e5ed8edb
jiaxu <jiaxuan938@gmail.com>
2024-11-04 18:59:05 +0800

    test commit

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -6,7 +6,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+//comment
 	private static final int SCREEN_WIDTH = 1300;
 	private static final int SCREEN_HEIGHT = 750;
 	private static final String TITLE = "Sky Battle";
