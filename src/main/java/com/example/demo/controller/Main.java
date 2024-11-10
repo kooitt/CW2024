a237f47342e290a503a14259c4ec21bc0fa77fa0
jkgithub02 <jkwork02@gmail.com>
2024-11-10 22:50:19 +0800

    save

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -4,13 +4,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class  Main extends Application {
