6a598793d5dde46598c3f8d4a400aee6f7aa9e10
davidjt <davidjt001@yahoo.com>
2024-11-12 06:17:45 +0800

    reformat LevelOne

--- src/main/java/com/example/demo/LevelOne.java
+++ src/main/java/com/example/demo/LevelOne.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 public class LevelOne extends LevelParent {
-	
+
 	private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background1.jpg";
 	private static final String NEXT_LEVEL = "com.example.demo.LevelTwo";
 	private static final int TOTAL_ENEMIES = 5;
@@ -17,8 +17,7 @@ public class LevelOne extends LevelParent {
 	protected void checkIfGameOver() {
 		if (userIsDestroyed()) {
 			loseGame();
-		}
-		else if (userHasReachedKillTarget()) {
+		} else if (userHasReachedKillTarget()) {
 			timeline.stop();
 			goToNextLevel(NEXT_LEVEL);
 		}
