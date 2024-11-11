66940aefac421554191bc2d75dc9c7e58370e2b9
davidjt <davidjt001@yahoo.com>
2024-11-12 06:17:23 +0800

    reformat GameOverImage

--- src/main/java/com/example/demo/GameOverImage.java
+++ src/main/java/com/example/demo/GameOverImage.java
@@ -6,12 +6,12 @@ import javafx.scene.image.ImageView;
 import java.util.Objects;
 
 public class GameOverImage extends ImageView {
-	
+
 	private static final String IMAGE_NAME = "/com/example/demo/images/gameover.png";
 
 	public GameOverImage(double xPosition, double yPosition) {
-		setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_NAME)).toExternalForm()) );
-//		setImage(ImageSetUp.getImageList().get(ImageSetUp.getGameOver()));
+		setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_NAME)).toExternalForm()));
+		//setImage(ImageSetUp.getImageList().get(ImageSetUp.getGameOver()));
 		setLayoutX(xPosition);
 		setLayoutY(yPosition);
 	}
