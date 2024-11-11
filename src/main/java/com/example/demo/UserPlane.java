f3a4ec3ce2a532d0935eafb5d8ac6a0c23a94b7a
davidjt <davidjt001@yahoo.com>
2024-11-12 05:33:03 +0800

    added X_LEFT_BOUND and X_RIGHT_BOUND in UserPlane

--- src/main/java/com/example/demo/UserPlane.java
+++ src/main/java/com/example/demo/UserPlane.java
@@ -5,6 +5,8 @@ public class UserPlane extends FighterPlane {
 	private static final String IMAGE_NAME = "userplane.png";
 	private static final double Y_UPPER_BOUND = -40;
 	private static final double Y_LOWER_BOUND = 600.0;
+	private static final double X_LEFT_BOUND = 0.0;
+	private static final double X_RIGHT_BOUND = 800.0; //TEMPORARY VALUE ONLY
 	private static final double INITIAL_X_POSITION = 5.0;
 	private static final double INITIAL_Y_POSITION = 300.0;
 	private static final int IMAGE_HEIGHT = 150;
