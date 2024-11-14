995fae72bdf8126a37a1184c6b4baceade3183eb
davidjt <davidjt001@yahoo.com>
2024-11-14 09:15:21 +0800

    cropped relevent images

---
+++ src/main/java/com/example/demo/Actors/ActiveActor.java
@@ -0,0 +1,31 @@
+package com.example.demo.Actors;
+
+import javafx.scene.image.*;
+
+import java.util.Objects;
+
+public abstract class ActiveActor extends ImageView {
+
+	private static final String IMAGE_LOCATION = "/com/example/demo/images/";
+
+	public ActiveActor(String imageName, int imageHeight, double initialXPos, double initialYPos) {
+		//this.setImage(new Image(IMAGE_LOCATION + imageName));
+		this.setImage(
+				new Image(Objects.requireNonNull(getClass().getResource(IMAGE_LOCATION + imageName)).toExternalForm()));
+		this.setLayoutX(initialXPos);
+		this.setLayoutY(initialYPos);
+		this.setFitHeight(imageHeight);
+		this.setPreserveRatio(true);
+	}
+
+	public abstract void updatePosition();
+
+	protected void moveHorizontally(double horizontalMove) {
+		this.setTranslateX(getTranslateX() + horizontalMove);
+	}
+
+	protected void moveVertically(double verticalMove) {
+		this.setTranslateY(getTranslateY() + verticalMove);
+	}
+
+}
