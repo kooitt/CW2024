2438303e0f1d75e3d30db5c6291f427f93b4571d
Visual2002 <viishal.avinash1@gmail.com>
2024-11-14 10:21:35 +0300

    added javadocs into main.java and controller.java

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -11,44 +11,82 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import com.example.demo.LevelParent;
 
+/**
+ * The Controller class is responsible for managing game levels and transitioning
+ * between them based on updates it receives. It serves as an Observer that
+ * listens for level changes and dynamically loads new levels.
+ */
 public class Controller implements Observer {
 
 	private static final String LEVEL_ONE_CLASS_NAME = "com.example.demo.LevelOne";
 	private final Stage stage;
 
+	/**
+	 * Constructs a Controller with the specified stage.
+	 *
+	 * @param stage The primary stage of the application where scenes are displayed.
+	 */
 	public Controller(Stage stage) {
 		this.stage = stage;
 	}
 
+	/**
+	 * Launches the game by displaying the stage and loading the first level.
+	 *
+	 * @throws ClassNotFoundException if the specified level class cannot be found.
+	 * @throws NoSuchMethodException if the specified constructor for the level class does not exist.
+	 * @throws SecurityException if access to the level class or its constructor is restricted.
+	 * @throws InstantiationException if the level class cannot be instantiated.
+	 * @throws IllegalAccessException if access to the level class or its constructor is not allowed.
+	 * @throws IllegalArgumentException if the constructor is invoked with inappropriate arguments.
+	 * @throws InvocationTargetException if the constructor throws an exception.
+	 */
 	public void launchGame() throws ClassNotFoundException, NoSuchMethodException, SecurityException,
-			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException  {
+			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 
-			stage.show();
-			goToLevel(LEVEL_ONE_CLASS_NAME);
+		stage.show();
+		goToLevel(LEVEL_ONE_CLASS_NAME);
 	}
 
+	/**
+	 * Loads and transitions to the specified game level.
+	 *
+	 * @param className The fully qualified class name of the level to load.
+	 * @throws ClassNotFoundException if the specified level class cannot be found.
+	 * @throws NoSuchMethodException if the specified constructor for the level class does not exist.
+	 * @throws SecurityException if access to the level class or its constructor is restricted.
+	 * @throws InstantiationException if the level class cannot be instantiated.
+	 * @throws IllegalAccessException if access to the level class or its constructor is not allowed.
+	 * @throws IllegalArgumentException if the constructor is invoked with inappropriate arguments.
+	 * @throws InvocationTargetException if the constructor throws an exception.
+	 */
 	private void goToLevel(String className) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
 			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-			Class<?> myClass = Class.forName(className);
-			Constructor<?> constructor = myClass.getConstructor(double.class, double.class);
-			LevelParent myLevel = (LevelParent) constructor.newInstance(stage.getHeight(), stage.getWidth());
-			myLevel.addObserver(this);
-			Scene scene = myLevel.initializeScene();
-			stage.setScene(scene);
-			myLevel.startGame();
-
+		Class<?> myClass = Class.forName(className);
+		Constructor<?> constructor = myClass.getConstructor(double.class, double.class);
+		LevelParent myLevel = (LevelParent) constructor.newInstance(stage.getHeight(), stage.getWidth());
+		myLevel.addObserver(this);
+		Scene scene = myLevel.initializeScene();
+		stage.setScene(scene);
+		myLevel.startGame();
 	}
 
+	/**
+	 * Called when the observed level notifies of a change, indicating a level transition.
+	 * Transitions to a new level based on the provided argument.
+	 *
+	 * @param arg0 The observable object; typically the current level.
+	 * @param arg1 The argument passed by the observable, typically the next level's class name.
+	 */
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		try {
 			goToLevel((String) arg1);
 		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException
-				| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				 | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setContentText(e.getClass().toString());
 			alert.show();
 		}
 	}
-
 }
--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -5,6 +5,10 @@ import java.lang.reflect.InvocationTargetException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * The Main class serves as the entry point of the application, setting up the
+ * main stage and initiating the game through the Controller class.
+ */
 public class Main extends Application {
 
 	private static final int SCREEN_WIDTH = 1300;
@@ -12,6 +16,19 @@ public class Main extends Application {
 	private static final String TITLE = "Sky Battle";
 	private Controller myController;
 
+	/**
+	 * Initializes and displays the main game window, setting its title, size, and
+	 * other properties. It then creates a Controller instance and starts the game.
+	 *
+	 * @param stage The primary stage for this application, where the main scene will be set.
+	 * @throws ClassNotFoundException if the initial level class cannot be found.
+	 * @throws NoSuchMethodException if the specified constructor for the initial level class does not exist.
+	 * @throws SecurityException if access to the level class or its constructor is restricted.
+	 * @throws InstantiationException if the initial level class cannot be instantiated.
+	 * @throws IllegalAccessException if access to the level class or its constructor is not allowed.
+	 * @throws IllegalArgumentException if the constructor is invoked with inappropriate arguments.
+	 * @throws InvocationTargetException if the constructor of the initial level class throws an exception.
+	 */
 	@Override
 	public void start(Stage stage) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
 			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
@@ -23,6 +40,12 @@ public class Main extends Application {
 		myController.launchGame();
 	}
 
+	/**
+	 * The main entry point of the application. It launches the JavaFX application
+	 * lifecycle, which will eventually call the start method.
+	 *
+	 * @param args Command line arguments passed to the application.
+	 */
 	public static void main(String[] args) {
 		launch();
 	}
