2c89bd56219a8d5f33bbd3ff81bd0804f50fe364
Ray <siaray88@gmail.com>
2024-11-19 17:09:34 +0800

    Fixed the position of gameover image to the centre for proper viewing as it was initially off-scale

--- src/main/java/com/example/demo/GameOverImage.java
+++ src/main/java/com/example/demo/GameOverImage.java
@@ -6,10 +6,12 @@ import javafx.scene.image.ImageView;
 public class GameOverImage extends ImageView {
 	
 	private static final String IMAGE_NAME = "/com/example/demo/images/gameover.png";
-
+	private static final double IMAGE_HEIGHT = 300;
+	private static final double IMAGE_WIDTH = 300;
 	public GameOverImage(double xPosition, double yPosition) {
 		setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()) );
-//		setImage(ImageSetUp.getImageList().get(ImageSetUp.getGameOver()));
+		setFitHeight(IMAGE_HEIGHT);
+		setFitWidth(IMAGE_WIDTH);
 		setLayoutX(xPosition);
 		setLayoutY(yPosition);
 	}
