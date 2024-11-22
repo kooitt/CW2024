de7cfe45936d19382b1d98fb1fbf2492bed22c9d
davidjt <davidjt001@yahoo.com>
2024-11-23 03:02:36 +0800

    UserPlane

--- src/main/java/com/example/demo/UserPlane.java
+++ src/main/java/com/example/demo/UserPlane.java
@@ -2,94 +2,94 @@ package com.example.demo;
 
 public class UserPlane extends FighterPlane {
 
-	private static final String IMAGE_NAME = "userplane.png";
-	private static final double Y_UPPER_BOUND = 20;
-	private static final double Y_LOWER_BOUND = 600.0;
-	private static final double X_LEFT_BOUND = 0.0;
-	private static final double X_RIGHT_BOUND = 800.0;
-	private static final double INITIAL_X_POSITION = 5.0;
-	private static final double INITIAL_Y_POSITION = 300.0;
-	private static final int IMAGE_HEIGHT = 39;
-	private static final int VERTICAL_VELOCITY = 8;
-	private static final int HORIZONTAL_VELOCITY = 8;
-	private static final int PROJECTILE_X_POSITION_OFFSET = 110;
-	private static final int PROJECTILE_Y_POSITION_OFFSET = 20;
-	private int verticalVelocityMultiplier;
-	private int horizontalVelocityMultiplier;
-	private int numberOfKills;
+    private static final String IMAGE_NAME = "userplane.png";
+    private static final double Y_UPPER_BOUND = 20;
+    private static final double Y_LOWER_BOUND = 600.0;
+    private static final double X_LEFT_BOUND = 0.0;
+    private static final double X_RIGHT_BOUND = 800.0;
+    private static final double INITIAL_X_POSITION = 5.0;
+    private static final double INITIAL_Y_POSITION = 300.0;
+    private static final int IMAGE_HEIGHT = 39;
+    private static final int VERTICAL_VELOCITY = 8;
+    private static final int HORIZONTAL_VELOCITY = 8;
+    private static final int PROJECTILE_X_POSITION_OFFSET = 110;
+    private static final int PROJECTILE_Y_POSITION_OFFSET = 20;
+    private int verticalVelocityMultiplier;
+    private int horizontalVelocityMultiplier;
+    private int numberOfKills;
 
-	public UserPlane(int initialHealth) {
-		super(IMAGE_NAME, IMAGE_HEIGHT, INITIAL_X_POSITION, INITIAL_Y_POSITION, initialHealth);
-		verticalVelocityMultiplier = 0;
-		horizontalVelocityMultiplier = 0;
-	}
+    public UserPlane(int initialHealth) {
+        super(IMAGE_NAME, IMAGE_HEIGHT, INITIAL_X_POSITION, INITIAL_Y_POSITION, initialHealth);
+        verticalVelocityMultiplier = 0;
+        horizontalVelocityMultiplier = 0;
+    }
 
-	@Override
-	public void updatePosition() {
-		if (isMoving()) {
-			double initialTranslateY = getTranslateY();
-			double initialTranslateX = getTranslateX();
+    @Override
+    public void updatePosition() {
+        if (isMoving()) {
+            double initialTranslateY = getTranslateY();
+            double initialTranslateX = getTranslateX();
 
-			this.moveVertically(VERTICAL_VELOCITY * verticalVelocityMultiplier);
-			this.moveHorizontally(HORIZONTAL_VELOCITY * horizontalVelocityMultiplier);
+            this.moveVertically(VERTICAL_VELOCITY * verticalVelocityMultiplier);
+            this.moveHorizontally(HORIZONTAL_VELOCITY * horizontalVelocityMultiplier);
 
-			double newYPosition = getLayoutY() + getTranslateY();
-			double newXPosition = getLayoutX() + getTranslateX();
+            double newYPosition = getLayoutY() + getTranslateY();
+            double newXPosition = getLayoutX() + getTranslateX();
 
-			if (newYPosition < Y_UPPER_BOUND || newYPosition > Y_LOWER_BOUND) {
-				this.setTranslateY(initialTranslateY);
-			}
-			if (newXPosition < X_LEFT_BOUND || newXPosition > X_RIGHT_BOUND) {
-				this.setTranslateX(initialTranslateX);
-			}
-		}
-	}
+            if (newYPosition < Y_UPPER_BOUND || newYPosition > Y_LOWER_BOUND) {
+                this.setTranslateY(initialTranslateY);
+            }
+            if (newXPosition < X_LEFT_BOUND || newXPosition > X_RIGHT_BOUND) {
+                this.setTranslateX(initialTranslateX);
+            }
+        }
+    }
 
-	@Override
-	public void updateActor() {
-		updatePosition();
-	}
+    @Override
+    public void updateActor() {
+        updatePosition();
+    }
 
-	@Override
-	public ActiveActorDestructible fireProjectile() {
-		return new UserProjectile(getProjectileXPosition(PROJECTILE_X_POSITION_OFFSET),
-				getProjectileYPosition(PROJECTILE_Y_POSITION_OFFSET));
-	}
+    @Override
+    public ActiveActorDestructible fireProjectile() {
+        return new UserProjectile(getProjectileXPosition(PROJECTILE_X_POSITION_OFFSET),
+                getProjectileYPosition(PROJECTILE_Y_POSITION_OFFSET));
+    }
 
-	private boolean isMoving() {
-		return verticalVelocityMultiplier != 0 || horizontalVelocityMultiplier != 0;
-	}
+    private boolean isMoving() {
+        return verticalVelocityMultiplier != 0 || horizontalVelocityMultiplier != 0;
+    }
 
-	public void moveUp() {
-		verticalVelocityMultiplier = -1;
-	}
+    public void moveUp() {
+        verticalVelocityMultiplier = -1;
+    }
 
-	public void moveDown() {
-		verticalVelocityMultiplier = 1;
-	}
+    public void moveDown() {
+        verticalVelocityMultiplier = 1;
+    }
 
-	public void moveLeft() {
-		horizontalVelocityMultiplier = -1;
-	}
+    public void moveLeft() {
+        horizontalVelocityMultiplier = -1;
+    }
 
-	public void moveRight() {
-		horizontalVelocityMultiplier = 1;
-	}
+    public void moveRight() {
+        horizontalVelocityMultiplier = 1;
+    }
 
-	public void stopVerticalMovement() {
-		verticalVelocityMultiplier = 0;
-	}
+    public void stopVerticalMovement() {
+        verticalVelocityMultiplier = 0;
+    }
 
-	public void stopHorizontalMovement() {
-		horizontalVelocityMultiplier = 0;
-	}
+    public void stopHorizontalMovement() {
+        horizontalVelocityMultiplier = 0;
+    }
 
-	public int getNumberOfKills() {
-		return numberOfKills;
-	}
+    public int getNumberOfKills() {
+        return numberOfKills;
+    }
 
-	public void incrementKillCount() {
-		numberOfKills++;
-	}
+    public void incrementKillCount() {
+        numberOfKills++;
+    }
 
 }
