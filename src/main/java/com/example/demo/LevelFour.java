c7a02edb0b215945c2c931e092e667c7454b0045
davidjt <davidjt001@yahoo.com>
2024-11-23 00:53:36 +0800

    added LevelFour

---
+++ src/main/java/com/example/demo/LevelFour.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+public class LevelFour extends LevelParent {
+
+    private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
+    private static final int PLAYER_INITIAL_HEALTH = 5;
+    private final Boss boss;
+    private LevelViewLevelTwo levelView;
+
+    public LevelFour(double screenHeight, double screenWidth) {
+        super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
+        boss = new Boss(levelView);
+    }
+
+    @Override
+    protected void initializeFriendlyUnits() {
+        getRoot().getChildren().add(getUser());
+    }
+
+    @Override
+    protected void checkIfGameOver() {
+        if (userIsDestroyed()) {
+            loseGame();
+        } else if (boss.isDestroyed()) {
+            // goToNextLevel(NEXT_LEVEL);
+            winGame();
+        }
+    }
+
+    @Override
+    protected void spawnEnemyUnits() {
+        if (getCurrentNumberOfEnemies() == 0) {
+            addEnemyUnit(boss);
+        }
+    }
+
+    @Override
+    protected LevelView instantiateLevelView() {
+        levelView = new LevelViewLevelTwo(getRoot(), PLAYER_INITIAL_HEALTH);
+        return levelView;
+    }
+
+}
