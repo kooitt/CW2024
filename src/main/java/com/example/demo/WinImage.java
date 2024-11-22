a73242cd5188eec8ec8e0846eadccfad3bfea655
davidjt <davidjt001@yahoo.com>
2024-11-23 03:03:21 +0800

    reformatted WinImage

--- src/main/java/com/example/demo/WinImage.java
+++ src/main/java/com/example/demo/WinImage.java
@@ -7,21 +7,21 @@ import java.util.Objects;
 
 public class WinImage extends ImageView {
 
-	private static final String IMAGE_NAME = "/com/example/demo/images/youwin.png";
-	private static final int HEIGHT = 500;
-	private static final int WIDTH = 600;
+    private static final String IMAGE_NAME = "/com/example/demo/images/youwin.png";
+    private static final int HEIGHT = 500;
+    private static final int WIDTH = 600;
 
-	public WinImage(double xPosition, double yPosition) {
-		this.setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_NAME)).toExternalForm()));
-		this.setVisible(false);
-		this.setFitHeight(HEIGHT);
-		this.setFitWidth(WIDTH);
-		this.setLayoutX(xPosition);
-		this.setLayoutY(yPosition);
-	}
+    public WinImage(double xPosition, double yPosition) {
+        this.setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_NAME)).toExternalForm()));
+        this.setVisible(false);
+        this.setFitHeight(HEIGHT);
+        this.setFitWidth(WIDTH);
+        this.setLayoutX(xPosition);
+        this.setLayoutY(yPosition);
+    }
 
-	public void showWinImage() {
-		this.setVisible(true);
-	}
+    public void showWinImage() {
+        this.setVisible(true);
+    }
 
 }
