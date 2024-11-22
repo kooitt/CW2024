cddc8d1d07d9b0b9749662d4b6f4239312535461
davidjt <davidjt001@yahoo.com>
2024-11-23 03:02:00 +0800

    reformatted Projectile

--- src/main/java/com/example/demo/Projectile.java
+++ src/main/java/com/example/demo/Projectile.java
@@ -2,16 +2,16 @@ package com.example.demo;
 
 public abstract class Projectile extends ActiveActorDestructible {
 
-	public Projectile(String imageName, int imageHeight, double initialXPos, double initialYPos) {
-		super(imageName, imageHeight, initialXPos, initialYPos);
-	}
+    public Projectile(String imageName, int imageHeight, double initialXPos, double initialYPos) {
+        super(imageName, imageHeight, initialXPos, initialYPos);
+    }
 
-	@Override
-	public void takeDamage() {
-		this.destroy();
-	}
+    @Override
+    public void takeDamage() {
+        this.destroy();
+    }
 
-	@Override
-	public abstract void updatePosition();
+    @Override
+    public abstract void updatePosition();
 
 }
