44ee956ca14502fdbfd1b572fc703282ad3636db
davidjt <davidjt001@yahoo.com>
2024-11-23 03:02:58 +0800

    reformatted UserProjectile

--- src/main/java/com/example/demo/UserProjectile.java
+++ src/main/java/com/example/demo/UserProjectile.java
@@ -2,22 +2,22 @@ package com.example.demo;
 
 public class UserProjectile extends Projectile {
 
-	private static final String IMAGE_NAME = "userfire.png";
-	private static final int IMAGE_HEIGHT = 6;
-	private static final int HORIZONTAL_VELOCITY = 15;
+    private static final String IMAGE_NAME = "userfire.png";
+    private static final int IMAGE_HEIGHT = 6;
+    private static final int HORIZONTAL_VELOCITY = 15;
 
-	public UserProjectile(double initialXPos, double initialYPos) {
-		super(IMAGE_NAME, IMAGE_HEIGHT, initialXPos, initialYPos);
-	}
+    public UserProjectile(double initialXPos, double initialYPos) {
+        super(IMAGE_NAME, IMAGE_HEIGHT, initialXPos, initialYPos);
+    }
 
-	@Override
-	public void updatePosition() {
-		moveHorizontally(HORIZONTAL_VELOCITY);
-	}
+    @Override
+    public void updatePosition() {
+        moveHorizontally(HORIZONTAL_VELOCITY);
+    }
 
-	@Override
-	public void updateActor() {
-		updatePosition();
-	}
+    @Override
+    public void updateActor() {
+        updatePosition();
+    }
 
 }
