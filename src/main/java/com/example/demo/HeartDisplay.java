7f05d40796db4b32e9d8eea725e7332e6f288287
davidjt <davidjt001@yahoo.com>
2024-11-23 02:57:31 +0800

    reformatted HeartDisplay

--- src/main/java/com/example/demo/HeartDisplay.java
+++ src/main/java/com/example/demo/HeartDisplay.java
@@ -8,46 +8,46 @@ import java.util.Objects;
 
 public class HeartDisplay {
 
-	private static final String HEART_IMAGE_NAME = "/com/example/demo/images/heart.png";
-	private static final int HEART_HEIGHT = 50;
-	private static final int INDEX_OF_FIRST_ITEM = 0;
-	private HBox container;
-	private final double containerXPosition;
-	private final double containerYPosition;
-	private final int numberOfHeartsToDisplay;
+    private static final String HEART_IMAGE_NAME = "/com/example/demo/images/heart.png";
+    private static final int HEART_HEIGHT = 50;
+    private static final int INDEX_OF_FIRST_ITEM = 0;
+    private final double containerXPosition;
+    private final double containerYPosition;
+    private final int numberOfHeartsToDisplay;
+    private HBox container;
 
-	public HeartDisplay(double xPosition, double yPosition, int heartsToDisplay) {
-		this.containerXPosition = xPosition;
-		this.containerYPosition = yPosition;
-		this.numberOfHeartsToDisplay = heartsToDisplay;
-		initializeContainer();
-		initializeHearts();
-	}
+    public HeartDisplay(double xPosition, double yPosition, int heartsToDisplay) {
+        this.containerXPosition = xPosition;
+        this.containerYPosition = yPosition;
+        this.numberOfHeartsToDisplay = heartsToDisplay;
+        initializeContainer();
+        initializeHearts();
+    }
 
-	private void initializeContainer() {
-		container = new HBox();
-		container.setLayoutX(containerXPosition);
-		container.setLayoutY(containerYPosition);
-	}
+    private void initializeContainer() {
+        container = new HBox();
+        container.setLayoutX(containerXPosition);
+        container.setLayoutY(containerYPosition);
+    }
 
-	private void initializeHearts() {
-		for (int i = 0; i < numberOfHeartsToDisplay; i++) {
-			ImageView heart = new ImageView(
-					new Image(Objects.requireNonNull(getClass().getResource(HEART_IMAGE_NAME)).toExternalForm()));
+    private void initializeHearts() {
+        for (int i = 0; i < numberOfHeartsToDisplay; i++) {
+            ImageView heart = new ImageView(
+                    new Image(Objects.requireNonNull(getClass().getResource(HEART_IMAGE_NAME)).toExternalForm()));
 
-			heart.setFitHeight(HEART_HEIGHT);
-			heart.setPreserveRatio(true);
-			container.getChildren().add(heart);
-		}
-	}
+            heart.setFitHeight(HEART_HEIGHT);
+            heart.setPreserveRatio(true);
+            container.getChildren().add(heart);
+        }
+    }
 
-	public void removeHeart() {
-		if (!container.getChildren().isEmpty())
-			container.getChildren().remove(INDEX_OF_FIRST_ITEM);
-	}
+    public void removeHeart() {
+        if (!container.getChildren().isEmpty())
+            container.getChildren().remove(INDEX_OF_FIRST_ITEM);
+    }
 
-	public HBox getContainer() {
-		return container;
-	}
+    public HBox getContainer() {
+        return container;
+    }
 
 }
