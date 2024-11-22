f88528bd46169be7f1bb1b79bb39b98cf6a18214
davidjt <davidjt001@yahoo.com>
2024-11-23 02:56:03 +0800

    reformatted EnemyProjectile

--- src/main/java/com/example/demo/EnemyProjectile.java
+++ src/main/java/com/example/demo/EnemyProjectile.java
@@ -2,22 +2,22 @@ package com.example.demo;
 
 public class EnemyProjectile extends Projectile {
 
-	private static final String IMAGE_NAME = "enemyFire.png";
-	private static final int IMAGE_HEIGHT = 32;
-	private static final int HORIZONTAL_VELOCITY = -10;
+    private static final String IMAGE_NAME = "enemyFire.png";
+    private static final int IMAGE_HEIGHT = 32;
+    private static final int HORIZONTAL_VELOCITY = -10;
 
-	public EnemyProjectile(double initialXPos, double initialYPos) {
-		super(IMAGE_NAME, IMAGE_HEIGHT, initialXPos, initialYPos);
-	}
+    public EnemyProjectile(double initialXPos, double initialYPos) {
+        super(IMAGE_NAME, IMAGE_HEIGHT, initialXPos, initialYPos);
+    }
 
-	@Override
-	public void updatePosition() {
-		moveHorizontally(HORIZONTAL_VELOCITY);
-	}
+    @Override
+    public void updatePosition() {
+        moveHorizontally(HORIZONTAL_VELOCITY);
+    }
 
-	@Override
-	public void updateActor() {
-		updatePosition();
-	}
+    @Override
+    public void updateActor() {
+        updatePosition();
+    }
 
 }
