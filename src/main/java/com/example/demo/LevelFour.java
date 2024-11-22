13802fcf95fc0687411bc60ef85e11563b42b116
davidjt <davidjt001@yahoo.com>
2024-11-23 00:59:57 +0800

    added boss object boss2 in LevelFour

--- src/main/java/com/example/demo/LevelFour.java
+++ src/main/java/com/example/demo/LevelFour.java
@@ -4,12 +4,14 @@ public class LevelFour extends LevelParent {
 
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
     private static final int PLAYER_INITIAL_HEALTH = 5;
-    private final Boss boss;
+    private final Boss boss1;
+    private final Boss boss2;
     private LevelViewLevelTwo levelView;
 
     public LevelFour(double screenHeight, double screenWidth) {
         super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
-        boss = new Boss(levelView);
+        boss1 = new Boss(levelView);
+        boss2 = new Boss(levelView);
     }
 
     @Override
