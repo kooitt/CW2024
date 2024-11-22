0c87d4407b2c1c8769eaaee8b96d9127b398994d
davidjt <davidjt001@yahoo.com>
2024-11-23 02:54:48 +0800

    reformatted Bossprojectile

--- src/main/java/com/example/demo/BossProjectile.java
+++ src/main/java/com/example/demo/BossProjectile.java
@@ -2,23 +2,23 @@ package com.example.demo;
 
 public class BossProjectile extends Projectile {
 
-	private static final String IMAGE_NAME = "fireball.png";
-	private static final int IMAGE_HEIGHT = 75;
-	private static final int HORIZONTAL_VELOCITY = -15;
-	private static final int INITIAL_X_POSITION = 950;
+    private static final String IMAGE_NAME = "fireball.png";
+    private static final int IMAGE_HEIGHT = 75;
+    private static final int HORIZONTAL_VELOCITY = -15;
+    private static final int INITIAL_X_POSITION = 950;
 
-	public BossProjectile(double initialYPos) {
-		super(IMAGE_NAME, IMAGE_HEIGHT, INITIAL_X_POSITION, initialYPos);
-	}
+    public BossProjectile(double initialYPos) {
+        super(IMAGE_NAME, IMAGE_HEIGHT, INITIAL_X_POSITION, initialYPos);
+    }
 
-	@Override
-	public void updatePosition() {
-		moveHorizontally(HORIZONTAL_VELOCITY);
-	}
+    @Override
+    public void updatePosition() {
+        moveHorizontally(HORIZONTAL_VELOCITY);
+    }
 
-	@Override
-	public void updateActor() {
-		updatePosition();
-	}
+    @Override
+    public void updateActor() {
+        updatePosition();
+    }
 
 }
