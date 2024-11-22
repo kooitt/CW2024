ad6fa5dd205728a3595da33484159aa7f952ade6
davidjt <davidjt001@yahoo.com>
2024-11-23 00:51:54 +0800

    added attribute NEXT_LEVEL in LevelThree

--- src/main/java/com/example/demo/LevelThree.java
+++ src/main/java/com/example/demo/LevelThree.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class LevelThree extends LevelParent {
 
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
+    private static final String NEXT_LEVEL = "com.example.demo.LevelFour";
     private static final int TOTAL_ENEMIES = 5;
     private static final double ENEMY_SPAWN_PROBABILITY = .20;
     private static final int PLAYER_INITIAL_HEALTH = 5;
