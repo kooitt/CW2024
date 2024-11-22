22c8ddfc0dbbdfed02e828850a84a59a85dfb039
davidjt <davidjt001@yahoo.com>
2024-11-23 02:56:41 +0800

    reformatted FighterPlane

--- src/main/java/com/example/demo/FighterPlane.java
+++ src/main/java/com/example/demo/FighterPlane.java
@@ -2,37 +2,37 @@ package com.example.demo;
 
 public abstract class FighterPlane extends ActiveActorDestructible {
 
-	private int health;
+    private int health;
 
-	public FighterPlane(String imageName, int imageHeight, double initialXPos, double initialYPos, int health) {
-		super(imageName, imageHeight, initialXPos, initialYPos);
-		this.health = health;
-	}
+    public FighterPlane(String imageName, int imageHeight, double initialXPos, double initialYPos, int health) {
+        super(imageName, imageHeight, initialXPos, initialYPos);
+        this.health = health;
+    }
 
-	public abstract ActiveActorDestructible fireProjectile();
+    public abstract ActiveActorDestructible fireProjectile();
 
-	@Override
-	public void takeDamage() {
-		health--;
-		if (healthAtZero()) {
-			this.destroy();
-		}
-	}
+    @Override
+    public void takeDamage() {
+        health--;
+        if (healthAtZero()) {
+            this.destroy();
+        }
+    }
 
-	protected double getProjectileXPosition(double xPositionOffset) {
-		return getLayoutX() + getTranslateX() + xPositionOffset;
-	}
+    protected double getProjectileXPosition(double xPositionOffset) {
+        return getLayoutX() + getTranslateX() + xPositionOffset;
+    }
 
-	protected double getProjectileYPosition(double yPositionOffset) {
-		return getLayoutY() + getTranslateY() + yPositionOffset;
-	}
+    protected double getProjectileYPosition(double yPositionOffset) {
+        return getLayoutY() + getTranslateY() + yPositionOffset;
+    }
 
-	private boolean healthAtZero() {
-		return health == 0;
-	}
+    private boolean healthAtZero() {
+        return health == 0;
+    }
 
-	public int getHealth() {
-		return health;
-	}
+    public int getHealth() {
+        return health;
+    }
 
 }
