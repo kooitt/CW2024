8636df3e5917dc04b1b6f46fa8f796e99ec87307
davidjt <davidjt001@yahoo.com>
2024-11-23 02:52:52 +0800

    reformat activeActorDestructible

--- src/main/java/com/example/demo/ActiveActorDestructible.java
+++ src/main/java/com/example/demo/ActiveActorDestructible.java
@@ -2,32 +2,32 @@ package com.example.demo;
 
 public abstract class ActiveActorDestructible extends ActiveActor implements Destructible {
 
-	private boolean isDestroyed;
+    private boolean isDestroyed;
 
-	public ActiveActorDestructible(String imageName, int imageHeight, double initialXPos, double initialYPos) {
-		super(imageName, imageHeight, initialXPos, initialYPos);
-		isDestroyed = false;
-	}
+    public ActiveActorDestructible(String imageName, int imageHeight, double initialXPos, double initialYPos) {
+        super(imageName, imageHeight, initialXPos, initialYPos);
+        isDestroyed = false;
+    }
 
-	@Override
-	public abstract void updatePosition();
+    @Override
+    public abstract void updatePosition();
 
-	public abstract void updateActor();
+    public abstract void updateActor();
 
-	@Override
-	public abstract void takeDamage();
+    @Override
+    public abstract void takeDamage();
 
-	@Override
-	public void destroy() {
-		setDestroyed();
-	}
+    @Override
+    public void destroy() {
+        setDestroyed();
+    }
 
-	protected void setDestroyed() {
-		this.isDestroyed = true;
-	}
+    protected void setDestroyed() {
+        this.isDestroyed = true;
+    }
 
-	public boolean isDestroyed() {
-		return isDestroyed;
-	}
-	
+    public boolean isDestroyed() {
+        return isDestroyed;
+    }
+
 }
