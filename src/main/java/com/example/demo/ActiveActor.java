60bbd6708b59b1abfd83948bac93d6b51e8f7a43
davidjt <davidjt001@yahoo.com>
2024-11-23 02:51:53 +0800

    reformat ActiveActor

--- src/main/java/com/example/demo/ActiveActor.java
+++ src/main/java/com/example/demo/ActiveActor.java
@@ -6,26 +6,26 @@ import java.util.Objects;
 
 public abstract class ActiveActor extends ImageView {
 
-	private static final String IMAGE_LOCATION = "/com/example/demo/images/";
+    private static final String IMAGE_LOCATION = "/com/example/demo/images/";
 
-	public ActiveActor(String imageName, int imageHeight, double initialXPos, double initialYPos) {
-		//this.setImage(new Image(IMAGE_LOCATION + imageName));
-		this.setImage(
-				new Image(Objects.requireNonNull(getClass().getResource(IMAGE_LOCATION + imageName)).toExternalForm()));
-		this.setLayoutX(initialXPos);
-		this.setLayoutY(initialYPos);
-		this.setFitHeight(imageHeight);
-		this.setPreserveRatio(true);
-	}
+    public ActiveActor(String imageName, int imageHeight, double initialXPos, double initialYPos) {
+        //this.setImage(new Image(IMAGE_LOCATION + imageName));
+        this.setImage(
+                new Image(Objects.requireNonNull(getClass().getResource(IMAGE_LOCATION + imageName)).toExternalForm()));
+        this.setLayoutX(initialXPos);
+        this.setLayoutY(initialYPos);
+        this.setFitHeight(imageHeight);
+        this.setPreserveRatio(true);
+    }
 
-	public abstract void updatePosition();
+    public abstract void updatePosition();
 
-	protected void moveHorizontally(double horizontalMove) {
-		this.setTranslateX(getTranslateX() + horizontalMove);
-	}
+    protected void moveHorizontally(double horizontalMove) {
+        this.setTranslateX(getTranslateX() + horizontalMove);
+    }
 
-	protected void moveVertically(double verticalMove) {
-		this.setTranslateY(getTranslateY() + verticalMove);
-	}
+    protected void moveVertically(double verticalMove) {
+        this.setTranslateY(getTranslateY() + verticalMove);
+    }
 
 }
