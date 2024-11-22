0cdc73a480dcde27dac03504d682b6d8d23d301a
davidjt <davidjt001@yahoo.com>
2024-11-23 00:29:47 +0800

    added LevelThree

---
+++ src/main/java/com/example/demo/LevelThree.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+public class LevelThree extends LevelParent {
+
+    private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
+    private static final int PLAYER_INITIAL_HEALTH = 5;
+    private final Boss boss;
+    private LevelViewLevelTwo levelView;
+
+    public LevelThree(double screenHeight, double screenWidth) {
+        super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
+        boss = new Boss(levelView);
+    }
+
+    @Override
+    protected void initializeFriendlyUnits() {
+        getRoot().getChildren().add(getUser());
+    }
+
+    @Override
+    protected void checkIfGameOver() {
+        if (userIsDestroyed()) {
+            loseGame();
+        } else if (boss.isDestroyed()) {
+            winGame();
+        }
+    }
+
+    @Override
+    protected void spawnEnemyUnits() {
+        if (getCurrentNumberOfEnemies() == 0) {
+            addEnemyUnit(boss);
+        }
+    }
+
+    @Override
+    protected LevelView instantiateLevelView() {
+        levelView = new LevelViewLevelTwo(getRoot(), PLAYER_INITIAL_HEALTH);
+        return levelView;
+    }
+
+}
