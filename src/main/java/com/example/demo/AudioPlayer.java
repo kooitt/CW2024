aeccdb874637d930ad33a5ebc8dd762f2e0eebaa
davidjt <davidjt001@yahoo.com>
2024-11-24 15:12:20 +0800

    added relevant methods in AudioPlayer

--- src/main/java/com/example/demo/AudioPlayer.java
+++ src/main/java/com/example/demo/AudioPlayer.java
@@ -6,4 +6,54 @@ import javafx.util.Duration;
 import java.io.File;
 
 public class AudioPlayer {
+    private MediaPlayer mediaPlayer;
+
+    // Load audio file
+    public void loadAudio(String filePath) {
+        Media media = new Media(new File(filePath).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+    }
+
+    // Play audio
+    public void play() {
+        if (mediaPlayer != null) {
+            mediaPlayer.play();
+        }
+    }
+
+    // Pause audio
+    public void pause() {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+        }
+    }
+
+    // Stop audio
+    public void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+    // Loop audio
+    public void loop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
+            mediaPlayer.play();
+        }
+    }
+
+    // Set volume
+    public void setVolume(double volume) {
+        if (mediaPlayer != null) {
+            mediaPlayer.setVolume(volume);
+        }
+    }
+
+    // Close audio resources
+    public void close() {
+        if (mediaPlayer != null) {
+            mediaPlayer.dispose();
+        }
+    }
 }
