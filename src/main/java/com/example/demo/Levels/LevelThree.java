c8b78d489935daf79b72280ae0512ba03768292a
davidjt <davidjt001@yahoo.com>
2024-11-28 03:32:13 +0800

    updated LevelThree to go to LevelFour after completion

--- src/main/java/com/example/demo/Levels/LevelThree.java
+++ src/main/java/com/example/demo/Levels/LevelThree.java
@@ -8,7 +8,7 @@ import com.example.demo.LevelViews.LevelView;
 public class LevelThree extends LevelParent {
 
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background1.jpg";
-    private static final String NEXT_LEVEL = "com.example.demo.Levels.LevelTwo";
+    private static final String NEXT_LEVEL = "com.example.demo.Levels.LevelFour";
     private static final int TOTAL_ENEMIES = 5;
     private static final int KILLS_TO_ADVANCE = 20;
     private static final double ENEMY_SPAWN_PROBABILITY = .20;
