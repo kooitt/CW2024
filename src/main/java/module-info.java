21a0c4686d2e6c9ef554df348f1d15a006eda76e
davidjt <davidjt001@yahoo.com>
2024-11-29 04:52:43 +0800

    updated module info to open Util and commented out line to open demo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,12 @@ module com.example.demo {
     requires javafx.media;
 
 
-    opens com.example.demo to javafx.fxml;
+    // opens com.example.demo to javafx.fxml; // to be removed later
     exports com.example.demo.controller;
     opens com.example.demo.Levels to javafx.fxml;
     opens com.example.demo.ImageInitializers to javafx.fxml;
     opens com.example.demo.LevelViews to javafx.fxml;
     opens com.example.demo.controller to javafx.fxml;
     opens com.example.demo.Actors to javafx.fxml;
+    opens com.example.demo.Util to javafx.fxml;
 }
