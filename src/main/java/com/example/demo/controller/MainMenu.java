219584240455ef49409a4e24a939d949590e2d85
davidjt <davidjt001@yahoo.com>
2024-11-28 12:16:58 +0800

    added background in MainMenu

--- src/main/java/com/example/demo/controller/MainMenu.java
+++ src/main/java/com/example/demo/controller/MainMenu.java
@@ -2,8 +2,15 @@ package com.example.demo.controller;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class MainMenu {
     private final Stage stage;
@@ -16,6 +23,14 @@ public class MainMenu {
     public Scene createMainMenuScene(Controller controller) {
         VBox menuLayout = new VBox(20);
         menuLayout.setStyle("-fx-alignment: center; -fx-padding: 50;");
+
+        Image backgroundImage = new Image(getClass().getResource("/com/example/demo/images/background1.jpg").toExternalForm());
+        BackgroundImage bgImage = new BackgroundImage(backgroundImage,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
+        menuLayout.setBackground(new Background(bgImage));
 
         Button playButton = new Button("Play");
         playButton.setOnAction(e -> controller.showLevelSelectionMenu());
