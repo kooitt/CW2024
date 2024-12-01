1b725732829df11f9f045767a270f01c2de8b463
Ray <siaray88@gmail.com>
2024-12-01 19:02:55 +0800

    Moved all Files into Subfolders & Made some other adjustments 1) Moved all files into subfolders in order to make things more neater and concise.
    2) Changed the speed of the user plane in order to promote a more exciting playtime
    
    3) Added hash set to keep track of active keys and prevent weird results happening from pressing two keys at once (i.e. Holding space to fire while moving would make the fire stutter and feel unrewarding/make the controls feel unresponsive). With hash set this was solved.
    
    4) Added a temporary 3rd level with adjusted difficulties for testing.

--- src/main/java/com/example/demo/actors/FighterPlane.java
+++ src/main/java/com/example/demo/actors/FighterPlane.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.actors;
 
 public abstract class FighterPlane extends ActiveActorDestructible {
 
@@ -10,7 +10,7 @@ public abstract class FighterPlane extends ActiveActorDestructible {
 	}
 
 	public abstract ActiveActorDestructible fireProjectile();
-	
+
 	@Override
 	public void takeDamage() {
 		health--;
@@ -34,5 +34,4 @@ public abstract class FighterPlane extends ActiveActorDestructible {
 	public int getHealth() {
 		return health;
 	}
-		
 }
