96f4d817686c76844bdfaa03bd87e38bf524d1ce
jiaxu <jiaxuan938@gmail.com>
2024-12-03 16:37:45 +0800

    refactor: integrate LevelParentController into Controller class
    - introduced `LevelParentController` to handle logic for interacting with `LevelParent` and its view
    - updated `goToLevel()` method to instantiate and pass the `LevelParentController` to manage the level's interactions
    - improved the structure by delegating control of level-related operations to `LevelParentController`

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
+import com.example.demo.LevelParentController;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -32,6 +33,7 @@ public class Controller implements Observer {
 			Constructor<?> constructor = myClass.getConstructor(double.class, double.class);
 			LevelParent myLevel = (LevelParent) constructor.newInstance(stage.getHeight(), stage.getWidth());
 			myLevel.addObserver(this);
+			LevelParentController levelController = new LevelParentController(myLevel, myLevel.getLevelView());
 			Scene scene = myLevel.initializeScene();
 			stage.setScene(scene);
 			myLevel.startGame();
